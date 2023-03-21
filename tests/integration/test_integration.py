697a39b45f8a9bdcf6845be061b34a0ebf2c07b6
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-03-21 18:21:53 +0100

    Added a cs.paml -> cs.html file test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -15,3 +15,11 @@ class TestPaml(unittest.TestCase):
         result = paml2html.convert_from_file(fpath)
         expected = ''
         self.assertEqual(result, expected)
+
+    def test_cs_file(self):
+        paml_path = Path(__file__).resolve().parent / 'fixtures' / 'cs.paml'
+        html_path = Path(__file__).resolve().parent / 'fixtures' / 'cs.html'
+        result = paml2html.convert_from_file(paml_path)
+        with open(html_path) as f:
+            expected = f.read()
+            self.assertEqual(result, expected)
