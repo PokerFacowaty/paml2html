2653ca993fc04433207ca0dc02f49d4fe2270f53
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-03-21 22:56:22 +0100

    Modified making paragraphs so that new lines can be indented

--- src/paml2html/paml2html.py
+++ src/paml2html/paml2html.py
@@ -350,7 +350,8 @@ def add_paragraph(paml_lines: list, i: int) -> int:
                 else:
                     text_to_add += paml_lines[i]
                 i += 1
-            text_to_add = '<br>'.join(text_to_add.splitlines())
+            text_to_add = '<br>'.join([line.lstrip()
+                                       for line in text_to_add.splitlines()])
             doc.asis(format_txt(text_to_add))
     i += 1
     return i
