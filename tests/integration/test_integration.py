7fb1a0773c2fed9a03f88506b82e18463b4fb894
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-03-21 03:59:29 +0100

    Added an empty file integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,4 +1,5 @@
 from src.paml2html import paml2html
+from pathlib import Path
 import unittest
 
 
@@ -6,5 +7,11 @@ class TestPaml(unittest.TestCase):
     def test_empty_text(self):
         text = ''
         result = paml2html.convert_from_text(text)
+        expected = ''
+        self.assertEqual(result, expected)
+
+    def test_empty_file(self):
+        fpath = Path(__file__).resolve().parent / 'fixtures' / 'empty.paml'
+        result = paml2html.convert_from_file(fpath)
         expected = ''
         self.assertEqual(result, expected)
