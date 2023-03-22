33feb6564aec36d3f34117ed601adb976fbf6131
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-03-22 17:47:25 +0100

    Added a description for text formatting tests

--- tests/other/test_text_formatting.py
+++ tests/other/test_text_formatting.py
@@ -1,2 +1,7 @@
 from src.paml2html import paml2html
 import unittest
+
+'''This is the place for all tests related to complex text formatting, such as
+   mixing text decorators, decorating links etc. They are separated from unit
+   tests as I, following the advice of my neighborhood tester, figured it
+   would look cleaner and more organized this way.'''
